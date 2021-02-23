d32c14a8883a727359b18b83511406779b16b35f
Clloyd3267 <37284048+Clloyd3267@users.noreply.github.com>
2021-02-22 20:15:49 -0500

    Occumap: Added getter and setters for datapoints

--- src/OccupancyGridMap.py
+++ src/OccupancyGridMap.py
@@ -40,36 +40,149 @@ class OccupancyGridMap:
     debug (bool):                 A variable to enable/disable debug outputs.
   """
 
-    def __init__(self, _grid_map=[], _cell_threshold=0.5, _cell_size=1, _debug=0):
-      """
-      The default constructor for class OccupancyGridMap.
+  def __init__(self, _grid_map=[], _cell_threshold=0.5, _cell_size=1, _debug=0):
+    """
+    The default constructor for class OccupancyGridMap.
 
-      Arguments:
-        _grid_map (matrix of float32): A datamap to use as the gridmap.
-        _cell_threshold (float32):     The threshold for a cell being occupied
-        _cell_size (float32):          The unitless size of a cell's length and width.
-        _debug (bool):                 Whether debug outputs are enabled.
-      """
+    Args:
+      _grid_map (matrix of float32): A datamap to use as the gridmap.
+      _cell_threshold (float32):     The threshold for a cell being occupied
+      _cell_size (float32):          The unitless size of a cell's length and width.
+      _debug (bool):                 Whether debug outputs are enabled.
+    """
 
-      self.grid_map       = _grid_map
-      self.cell_threshold = _cell_threshold
-      self.cell_size      = _cell_size
-      self.debug          = _debug
+    self.grid_map       = _grid_map
+    self.cell_threshold = _cell_threshold
+    self.cell_size      = _cell_size
+    self.debug          = _debug
 
   # ----------------------------------------------------------------------------
   # Class Methods
   # ----------------------------------------------------------------------------
+
+  def getDataLoc(self, loc_point):
+    """
+    Method to get the occupancy value at a certain coordinate location.
+
+    Args:
+      loc_point (tuple of float32): A real world point (x, y). Scaled by _cell_size.
+
+    Returns:
+      value (float32): The value of occupancy.
+    """
+
+    # Get the index of the given point
+    index_point = self.locToIndex(loc_point)
+
+    # Get the value of occupancy
+    return self.getDataIndex(index_point)
+
+  def getDataIndex(self, index_point):
+    """
+    Method to get the occupancy value at a certain index location.
+
+    Args:
+      index_point (tuple of float32): An indexed point (x, y).
+
+    Returns:
+      value (float32): The value of occupancy.
+    """
+
+    # Ensure the point is valid
+    if (not self.isValidIndexPoint(index_point)):
+      print("ERROR: Entered point is invalid!")
+      return -1 # CDL=> Should be exception thrown?
+
+    # Get index point x,y
+    x, y = index_point
+
+    # Get the value of occupancy
+    return self.grid_map[y][x]
+
+  def setDataLoc(self, loc_point, value):
+    """
+    Method to set the occupancy value at a certain coordinate location.
+
+    Args:
+      loc_point (tuple of float32): A real world point (x, y). Scaled by _cell_size.
+      value (float32):              The value of occupancy to set.
+
+    Returns:
+      value (float32): The value of occupancy.
+    """
+
+    # Get the index of the given point
+    index_point = self.locToIndex(loc_point)
+
+    # Set the occupancy value
+    return setDataIndex(index_point, value)
+
+
+  def setDataIndex(self, index_point, value):
+    """
+    Method to set the occupancy value at a certain index location.
+
+    Args:
+      index_point (tuple of float32): An indexed point (x, y).
+      value (float32):                The value of occupancy to set.
+
+    Returns:
+      value (float32): The value of occupancy.
+    """
+
+    # Ensure the point is valid
+    if (not self.isValidIndexPoint(index_point)):
+      print("ERROR: Entered point is invalid!")
+      return -1 # CDL=> Should be exception thrown?
+
+    # Get index point x,y
+    x, y = index_point
+
+    # CDL=> Add logic to validate value
+
+    # Set the occupancy value
+    self.grid_map[y][x] = value
+
+    return value
+
+
+
+
+
+
+
+
+
+
+    # Get index point x,y
+    x, y = index_point
+
+    # Ensure the point is valid # CDL=> Check if conditional works
+    if ((x < 0) or
+        (y < 0) or
+        (np.size(self.grid_map, 1) <= x) or
+        (np.size(self.grid_map, 0) <= y)):
+
+
+
   # CDL=> Here: List of methods
   #
-  # getDataLoc(self, loc_point)
-  # getDataIndex(self, index_point)
-  # setDataIndex(self, index_point, value)
-  # setDataLoc(self, loc_point, value)
-  # isOccupiedLoc(self, loc_point)
-  # isOccupiedIndex(self, index_point)
-  # indexToLoc(self, index_point)
-  # locToIndex(self, loc_point)
-  # visualizeGrid(self, config)
+  # [X] getDataLoc(self, loc_point)
+  # [X] getDataIndex(self, index_point)
+  # [X] setDataLoc(self, loc_point, value)
+  # [X] setDataIndex(self, index_point, value)
+  # [ ] isOccupiedLoc(self, loc_point)
+  # [ ] isOccupiedIndex(self, index_point)
+  # [ ] isUnoccupiedLoc(self, loc_point)
+  # [ ] isUnoccupiedIndex(self, index_point)
+  # [ ] isUnknownLoc(self, loc_point)
+  # [ ] isUnknownIndex(self, index_point)
+  # [ ] isValidLocPoint(self, loc_point)
+  # [ ] isValidIndexPoint(self, index_point)
+  # [ ] indexToLoc(self, index_point)
+  # [ ] locToIndex(self, loc_point)
+  # [ ] visualizeGrid(self, config)
+  # [ ] fromCSV()
 
 if __name__ == "__main__":
   print("Hello World!") # CDL=> Add simple example of class
